35b6521539108bc0856bb1b0751f630e4d394c13
zhahahahan <a114221066@mail.shu.edu.tw>
2025-12-10 11:33:16 +0800

    114_12_10

---
+++ 114_12_10/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            if (lightOnPictureBox.Visible == true)
+
+
+            {
+                lightOnPictureBox.Visible = false;
+                lightOffPictureBox.Visible = true;
+                lightStateLabel.Text = "OFF";
+            }
+            else
+            {
+                lightOnPictureBox.Visible = true;
+                lightOffPictureBox.Visible = false;
+                lightStateLabel.Visible = "ON";
+                
+            }
+
+           
+        }
+
+        private 
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 114_12_10/Tutorial 6-2/Cards/Cards/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showCardButton_Click(object sender, EventArgs e)
+        {
+            if (cardListBox.SelectedIndex != -1)
+            {
+                showSelectedCard(cardListBox.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("請先選擇一張卡片!");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
